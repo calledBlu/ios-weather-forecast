c310725891de7da00f8262fbf7688aaef60ae4ba
ueunli <cdenen20@gmail.com>
2023-03-30 17:05:06 +0900

    [작업중] DTO 관련 리팩토링
    - dataTime을 String이 아닌 Double값으로 받아오도록 수정
    - 날짜, 장소 포맷을 다듬기 위한 열거형 구현

--- WeatherForecast/WeatherForecast/Model/WeatherData/CurrentWeatherData.swift
+++ WeatherForecast/WeatherForecast/Model/WeatherData/CurrentWeatherData.swift
@@ -6,9 +6,31 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct CurrentWeatherData {
     let icon: String
-    let location: String
+    let location: String = "loading..."
     let temperature, minimumTemperature, maximumTemperature: Double
+    
+    init(_ weatherData: CurrentWeatherComponents) {
+        self.icon = weatherData.weather.first!.icon
+        self.temperature = weatherData.numericalInformation.temperature
+        self.minimumTemperature = weatherData.numericalInformation.minimumTemperature
+        self.maximumTemperature = weatherData.numericalInformation.maximumTemperature
+    }
+}
+
+enum LocationFormatter {
+    private static let geocoder = CLGeocoder()
+    
+    private static func parse(_ coordinate: CLLocation) async throws -> CLPlacemark {
+        let names = try await geocoder.reverseGeocodeLocation(coordinate)
+        return names.first!
+    }
+    
+    static func make(_ coordinate: CLLocation) async throws -> String {
+        let name = try await parse(coordinate)
+        return name.administrativeArea! + " " + name.thoroughfare!
+    }
 }
